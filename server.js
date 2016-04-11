ba0fbe578689886935002aba0e939f6b9c23d82a
Leandro Ramos <leandro.ramos@cancaonova.com>
2016-04-11 11:48:54 -0300

    Inclusão de socket.io

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ var app         =   express();
 var mongoose = require('mongoose');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var gcm = require('node-gcm');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(bodyParser.json());
 app.use(cors());
 
 var usersController = require('./static/Users/controller');
 
-var url = 'mongodb://root:#q1w2e3#@ds019970.mlab.com:19970/chat';
-mongoose.connect(url);
+mongoose.connect(process.env.MONGODB_URL);
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+/*
+if (io.sockets.connected[socketid]) {
+    io.sockets.connected[socketid].emit('message', 'for your eyes only');
+}
+*/
 
 app.get('/api/users/list', usersController.list);
 
